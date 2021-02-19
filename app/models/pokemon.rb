896db3559eb582b6882ace96c00f75c7bed4e1dd
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2021-02-18 23:00:03 -0300

    added pokedex_id unique field to  pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,7 @@
 class Pokemon
   include Mongoid::Document
+  field :pokedex_id, type: Integer
   field :name, type: String
   field :base_experience, type: Integer
+  field :_id, type: Integer, default: -> { pokedex_id }
 end
