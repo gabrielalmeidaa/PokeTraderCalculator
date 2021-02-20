e8fb749567baa0085a9bf964cfb0db5681760a9e
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2021-02-20 14:43:11 -0300

    Add specs for TradeOffer model

--- app/models/trade_offer.rb
+++ app/models/trade_offer.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 class TradeOffer
   include Mongoid::Document
   field :offer_total_experience, type: Integer
   embeds_many :pokemons
+
+  validate :pokemon_offer_list_not_empty
+  after_create :calculate_trade_offer_total_experience
+
+  def calculate_trade_offer_total_experience
+    self.offer_total_experience = pokemons.reduce(0) { |total_exp, pokemon| total_exp + pokemon.base_experience }
+  end
+
+  private
+
+  def pokemon_offer_list_not_empty
+    return unless pokemons.empty?
+
+    errors.add(:pokemons, 'Pokemon list on trade offer should not be empty.')
+  end
 end
---
+++ spec/models/trade_offer_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TradeOffer do
+  let(:pokemons){
+    [
+      create(:pokemon, pokedex_id: 1, name: 'charizard', base_experience: 10),
+      create(:pokemon, pokedex_id: 2, name: 'mewtwo', base_experience: 20),
+      create(:pokemon, pokedex_id: 3, name: 'ghastly', base_experience: 100)
+    ]
+  }
+  let(:subject) { described_class.new(offer_total_experience: 0, pokemons: pokemons) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid with empty pokemon array' do
+    subject.pokemons = []
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid with not Pokemon type object on pokemon array' do
+    subject.pokemons.append(build(:trade_offer))
+    expect(subject).not_to be_valid
+  end
+
+  it 'correctly calculates total_experience after object_creation' do
+    subject.save
+    expect(subject.offer_total_experience).to eq(130)
+  end
+
+end
