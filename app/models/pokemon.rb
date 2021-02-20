f12c7be2a5239d18f4870c0e4462c7774f49e4ce
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2021-02-19 22:37:30 -0300

    added attributes validation to pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -4,4 +4,8 @@ class Pokemon
   field :name, type: String
   field :base_experience, type: Integer
   field :_id, type: Integer, default: -> { pokedex_id }
+
+  validates_presence_of :name, :base_experience, :pokedex_id
+  validates_uniqueness_of :pokedex_id
+
 end
