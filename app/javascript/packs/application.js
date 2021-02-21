a142305b00ea076c3c105d6ac3fa13b9fa2ac363
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2021-02-21 09:52:10 -0300

    add select2 to js requirements

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,7 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
-// require("stisla")
-// require("scripts")
-// require("custom")
+require("select2")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
