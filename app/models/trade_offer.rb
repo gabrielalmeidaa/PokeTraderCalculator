66335722c49498d22bcb11a3dcb46fe560cabaea
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2021-02-21 16:14:32 -0300

    add spec for maxmimum pokemon length validation

--- app/models/trade_offer.rb
+++ app/models/trade_offer.rb
@@ -2,10 +2,13 @@
 
 class TradeOffer
   include Mongoid::Document
+  POKEMON_PER_TRADE_LIMIT = 6
+
   has_and_belongs_to_many :pokemons, inverse_of: nil
 
   embeds_many :pokemons
   validate :pokemon_offer_list_not_empty
+  validate :pokemon_offer_list_does_not_surpass_pokemon_limit
   validate :only_pokemon_instances_on_pokemon_array
 
   def offer_total_experience
@@ -13,6 +16,12 @@ class TradeOffer
   end
 
   private
+
+  def pokemon_offer_list_does_not_surpass_pokemon_limit
+    return unless pokemons.length > POKEMON_PER_TRADE_LIMIT
+
+    errors.add(:pokemons, 'Pokemon offer list should not be greater than #{POKEMON_PER_TRADE_LIMIT}')
+  end
 
   def pokemon_offer_list_not_empty
     return unless pokemons.empty?
--- spec/models/trade_offer_spec.rb
+++ spec/models/trade_offer_spec.rb
@@ -5,11 +5,12 @@ require 'rails_helper'
 RSpec.describe TradeOffer do
   let(:pokemons)  do
     [
-      create(:pokemon, _id: 1, pokedex_id: 1, name: 'charizard', base_experience: 10),
-      create(:pokemon, _id: 2, pokedex_id: 2, name: 'mewtwo', base_experience: 20),
-      create(:pokemon, _id: 3, pokedex_id: 3, name: 'ghastly', base_experience: 100)
+      create(:pokemon, pokedex_id: 1, name: 'charizard', base_experience: 10),
+      create(:pokemon, pokedex_id: 2, name: 'mewtwo', base_experience: 20),
+      create(:pokemon, pokedex_id: 3, name: 'ghastly', base_experience: 100)
     ]
   end
+
   let(:subject) { described_class.new(pokemons: pokemons) }
 
   it 'is valid with valid attributes' do
@@ -21,13 +22,26 @@ RSpec.describe TradeOffer do
     expect(subject).not_to be_valid
   end
 
+  it 'is not valid with pokemon offer array longer than limit' do
+    4.times do
+      subject.pokemons.append(build(:pokemon))
+    end
+    expect(subject).not_to be_valid
+  end
+
   it 'is not valid with not Pokemon type object on pokemon array' do
     subject.pokemons = [build(:trade_offer, pokemons: pokemons)]
     expect(subject).not_to be_valid
   end
 
-  it 'correctly calculates total_experience after object_creation' do
-    subject.save
-    expect(subject.offer_total_experience).to eq(130)
+  describe '.offer_total_experience' do
+    it 'correctly calculates total_experience' do
+      expect(subject.offer_total_experience).to eq(130)
+    end
+
+    it 'correctly calculates total_experience with repeated pokemon on list' do
+      subject.pokemons.append(pokemons.last)
+      expect(subject.offer_total_experience).to eq(230)
+    end
   end
 end
